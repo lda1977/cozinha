376cf290a2e40375db226f8e3c6232b5a8f48c7f
Administrador <lucianoa2028@gmail.com>
2022-07-26 20:03:21 -0300

    Quinto Commit 1 - Incluido da Tela activity_main_sistema.xml; 2 - Nova tela activity_home_cozinha.xml.

---
+++ app/src/main/java/com/ptz/cozinha/MainActivitySistema.kt
@@ -0,0 +1,11 @@
+package com.ptz.cozinha
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivitySistema : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_sistema)
+    }
+}
